eecba2451a197f608dc629b456350b38a41e1f97
lschacon1988 <lschacon1988@gmail.com>
2022-08-29 11:58:13 -0500

    api

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import dotenv from'dotenv'
+dotenv.config()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
 root.render(
   <React.StrictMode>
     
