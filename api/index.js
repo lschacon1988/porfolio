e598ad4d1e3b264c9ef4e5b147ff04244d217206
lschacon1988 <lschacon1988@gmail.com>
2022-08-29 15:17:27 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3001, () => console.log("Server Running"));
+app.listen(process.env.PORT || 3001, () => console.log("Server Running"));
 
 
 const contactEmail = nodemailer.createTransport({
