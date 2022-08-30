40bfde328c3b78c67840b1f6b0a4e9b723aac0db
lschacon1988 <lschacon1988@gmail.com>
2022-08-30 08:54:20 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,11 +6,12 @@ require("dotenv").config();
 const { EMAIL, PASSWORD_EMAIL } = process.env;
 
 // server used to send send emails
+const PORT = process.env.PORT || 3001
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(process.env.PORT || 3001, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running", PORT));
 
 
 const contactEmail = nodemailer.createTransport({
