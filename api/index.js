88983dbf6811a0e21d876356b03bb6364ba7b801
lschacon1988 <lschacon1988@gmail.com>
2022-08-31 15:53:12 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors");
+//const cors = require("cors");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const { EMAIL, PASSWORD_EMAIL } = process.env;
@@ -8,7 +8,7 @@ const { EMAIL, PASSWORD_EMAIL } = process.env;
 // server used to send send emails
 const PORT = process.env.PORT || 3001
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(PORT, () => console.log("Server Running", PORT));
