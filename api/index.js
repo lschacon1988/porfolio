346b14139cab5620c317c76120c02440cd409c5f
lschacon1988 <lschacon1988@gmail.com>
2022-08-31 16:04:43 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-//const cors = require("cors");
+const morgan = require('morgan')
+
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const { EMAIL, PASSWORD_EMAIL } = process.env;
@@ -8,7 +11,9 @@ const { EMAIL, PASSWORD_EMAIL } = process.env;
 // server used to send send emails
 const PORT = process.env.PORT || 3001
 const app = express();
-//app.use(cors());
+app.use(cors());
+app.use(cookieParser())
+app.use(morgan('dev'))
 app.use(express.json());
 app.use("/", router);
 app.listen(PORT, () => console.log("Server Running", PORT));
