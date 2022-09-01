5d8729f8b24d7d53bf15fcf9cad68959400e0548
lschacon1988 <lschacon1988@gmail.com>
2022-09-01 09:32:45 -0500

    Update Contact.jsx

--- client/src/componet/Contact.jsx
+++ client/src/componet/Contact.jsx
@@ -27,7 +27,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await axios.post("https://porfolioapp.vercel.app/contact", formDetails
+    let response = await axios.post("https://porfolio-production.up.railway.app/contact", formDetails
     //  {
     //   method: "POST",
     //   headers: {
